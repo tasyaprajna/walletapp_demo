bde06d0eab0d824302bd71e5524b2b5e6543ac4d
tasyaprajna <prajna.tasya@binus.ac.id>
2021-04-20 13:55:26 +0700

    inisialisasi walletapp_demo project

---
+++ src/main/java/org/bsim/intern/WalletappDemoApplication.java
@@ -0,0 +1,13 @@
+package org.bsim.intern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalletappDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WalletappDemoApplication.class, args);
+    }
+
+}
